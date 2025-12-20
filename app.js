4f2c3c67d305fab347539397071915994efb0f5c
binayakbhandari <binayakbhandari60@gmail.com>
2025-12-20 08:27:15 +0545

    Project Online

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 // Cors configuration
 app.use(cors({
-    origin: ['https://book-management-brown.vercel.app/']
+    origin: ['https://book-management-brown.vercel.app']
 }))
 
 
